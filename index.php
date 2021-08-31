68e90b4beb5ce47ece06c24fd2f392cf6480b98d
Sandeep Kavalur <sandeep.kavalur@Indegene.com>
2021-08-31 22:09:35 +0530

    csv fetch

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="index.css" type="text/css" rel="stylesheet">
 </head>
 <body>
     <form method="POST" enctype="multipart/form-data">
@@ -20,11 +21,10 @@
         Email: <input type="email" name="email"><br>
         <input type="file" name="file" ><br>
         <button type="submit">submit</button><br>
-        <a href="./index.txt" download="resume" >download file?</a><br>
+        <a href="./index.txt" download="resume" >download resume file?</a><br>
     </form>
     <?php 
         if(key_exists('file', $_FILES)){
-            echo $_FILES["file"]['name']."<br>";
             $tmp_name = $_FILES["file"]['tmp_name'];
             $file_name = $_FILES['file']['name'];
             $dir = getcwd().'/'.$file_name;
@@ -37,13 +37,46 @@
             $content = $fname."\n".$lname."\n".$expi."\n".$email;
             
             if(move_uploaded_file($tmp_name, $dir)){
-                echo "File uploaded successfully!";
+                echo "File uploaded successfully! <br>";
             } else {
-                echo "Sorry, file not uploaded";
+                echo "Sorry, file not uploaded <br>";
             }
             
-            $myfile = fopen('index.txt', "w+");
-            fwrite($myfile, $content);
+            $myfile = fopen('resume.csv', "a+");
+            $form_data = array(
+                'First name' => $fname,
+                'Last name' => $lname,
+                'Experience' => $expi,
+                'Email' => $email
+            );
+            fputcsv($myfile, $form_data);
+            fclose($myfile);
+
+            $myfile = fopen('resume.csv', "r");
+            $row = 1;
+            echo "
+            <table class='tabl'>
+                <tr>
+                    <th>First name</th>
+                    <th>Last name</th>
+                    <th>Experience</th>
+                    <th>Email</th>
+                </tr>
+            ";
+            while (($data = fgetcsv($myfile, 1000, ",")) !== FALSE) {
+                $num = count($data);
+                echo "<tr>";
+                
+                $row++;
+                for ($c=0; $c < $num; $c++) {
+                    echo "
+                        <td>$data[$c]</td>
+                    ";
+                }
+                echo "</tr>";
+            }
+            echo "</table>";
+
             fclose($myfile);
         }
     ?>
